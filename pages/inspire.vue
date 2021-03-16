d1ecce8583ac8d8451ab1ecac7db3b6c158ed647
Pedro Gaudêncio <pmgaudencio@gmail.com>
2021-03-16 12:36:03 +0000

    Initial Vue code + iOS app

---
+++ pages/inspire.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <p v-for="(v, k) in testAxios" :key="v">
+        {{ k }}: {{ v }}
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+
+<script>
+export default {
+  name: "inspire",
+  data() {
+    return {
+      testAxios: null,
+    };
+  },
+  methods: {
+    async axiosTest() {
+      this.testAxios = await this.$axios.$get('todos/1')
+    }
+  },
+  mounted() {
+    this.axiosTest()
+  }
+}
+</script>
